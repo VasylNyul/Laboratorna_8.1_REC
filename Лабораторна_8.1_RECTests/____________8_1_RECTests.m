6c16580af833d91ca4cf0a234649ba8e31bf58ad
Nyul <Vasyl.Niul.IT.2021@lpnu.ua>
2021-12-01 10:29:19 +0200

    Initial Commit

---
+++ Лабораторна_8.1_RECTests/____________8_1_RECTests.m
@@ -0,0 +1,36 @@
+//
+//  ____________8_1_RECTests.m
+//  Лабораторна_8.1_RECTests
+//
+//  Created by apple on 01.12.2021.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ____________8_1_RECTests : XCTestCase
+
+@end
+
+@implementation ____________8_1_RECTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
